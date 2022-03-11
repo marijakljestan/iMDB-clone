71161e699eb446ac89e0c2bc4bafb6ea62affc5e
Marija Klještan <marijakljestan@uns.ac.rs>
2022-03-11 10:38:28 +0100

    feat: Configuring routing

--- Application/Frontend/src/app/app.module.ts
+++ Application/Frontend/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movies/movie-list/movie-list.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { ImageGalleryComponent } from './shared/image-gallery/image-gallery.component';
+import { MoviePageComponent } from './movies/movie-page/movie-page.component';
+import { HomePageComponent } from './home/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
     NavbarComponent,
-    ImageGalleryComponent
+    ImageGalleryComponent,
+    MoviePageComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+       { path: 'movie/:id', component: MoviePageComponent},
+       { path: 'home', component: HomePageComponent },
+       { path: '', redirectTo: 'home', pathMatch: 'full'},
+       { path: '**', redirectTo: 'home', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
---
+++ Application/Frontend/src/app/home/home-page.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  topRatedList: any[] = [];
+  watchList: any[] = [];
+  trendingList: any[] = [];
+  images: string[] = [];
+
+  ngOnInit() : void {
+    this.topRatedList = [
+      {
+        name: 'The Godfather',
+        averageGrade : 9.1,
+        image: "../../../assets/images/godfather.jpg"
+      },
+      {
+        name: 'The Dark Knight',
+        averageGrade : 8.8,
+        image: "../../../assets/images/dark-knight.jfif"
+      },
+      {
+        name: 'Fight club',
+        averageGrade : 8.7,
+        image: "../../../assets/images/fight-club.jpg"
+      },
+      {
+        name: 'Inception',
+        averageGrade : 8.6,
+        image: "../../../assets/images/inception.jpg"
+      },
+      {
+        name: 'Life is beautiful',
+        averageGrade : 8.5,
+        image: "../../../assets/images/lavitaebella.jpg"
+      },
+      {
+        name: "Schindler's List",
+        averageGrade : 8.7,
+        image: "../../../assets/images/schindler.jpg"
+      },
+      {
+        name: 'Pulp Fiction',
+        averageGrade : 8.5,
+        image: "../../../assets/images/pulp fiction.jpg"
+      },
+      {
+        name: 'Joker',
+        averageGrade : 8.3,
+        image: "../../../assets/images/joker.jpg"
+      }
+    ];
+
+    this.trendingList = [
+      {
+        name: 'Shawshank Redemption',
+        averageGrade : 9.2,
+        image: "../../../assets/images/shawshank.jfif"
+      },
+      {
+        name: "Schindler's List",
+        averageGrade : 8.7,
+        image: "../../../assets/images/schindler.jpg"
+      },
+      {
+        name: 'Pulp Fiction',
+        averageGrade : 8.5,
+        image: "../../../assets/images/pulp fiction.jpg"
+      },
+      {
+        name: 'Joker',
+        averageGrade : 8.3,
+        image: "../../../assets/images/joker.jpg"
+      },
+      {
+        name: 'Shutter Island',
+        averageGrade : 8.1,
+        image: "../../../assets/images/shutter.jfif"
+      }
+    ];
+
+    this.images = [
+      "../../../assets/images/cover/batman.jpg",
+      "../../../assets/images/cover/dark-knight.jpg",
+      "../../../assets/images/cover/fight-club.jpg",
+      "../../../assets/images/cover/inception.jpg",
+      "../../../assets/images/cover/la-vitta-e-bella.jpg",
+      "../../../assets/images/cover/godfather.jpg"
+    ];
+  }
+}
