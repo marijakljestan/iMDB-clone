d448b9d575a91785009144f13c9b1653fec8e538
Marija Klještan <marijakljestan@uns.ac.rs>
2022-03-17 23:03:22 +0100

    feat: Login and signup modals [Frontend]

--- Application/Frontend/src/app/shared/navbar/navbar.component.ts
+++ Application/Frontend/src/app/shared/navbar/navbar.component.ts
@@ -8,4 +8,22 @@ import { Component} from '@angular/core';
 export class NavbarComponent {
   title : string = 'CINEMATIC';
 
+  openLoginModal(event: MouseEvent): void {
+    event.preventDefault();
+    (document.querySelector('#log-in-modal') as HTMLElement).style.display = 'flex';
+  }
+
+  openSignUpModal(event: MouseEvent): void {
+    event.preventDefault();
+    (document.querySelector('#sign-up-modal') as HTMLElement).style.display = 'flex';
+  }
+
+  closeLoginModal() : void{
+    (document.querySelector('#log-in-modal') as HTMLElement).style.display = 'none';
+  }
+
+  closeSignupModal() : void{
+    (document.querySelector('#sign-up-modal') as HTMLElement).style.display = 'none';
+  }
+
 }
