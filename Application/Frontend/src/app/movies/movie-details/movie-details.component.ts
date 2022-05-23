9eeb96a5bb288b0241c88990dea82bc83134c206
Marija Klještan <marijakljestan@uns.ac.rs>
2022-05-23 13:22:54 +0200

    feat: Add/remove from watchlist - movie page

--- Application/Frontend/src/app/movies/movie-details/movie-details.component.ts
+++ Application/Frontend/src/app/movies/movie-details/movie-details.component.ts
@@ -1,15 +1,42 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MovieDto } from 'src/app/model/movie-dto.model';
+import { User } from 'src/app/model/user.model';
+import { MovieService } from 'src/app/service/movie.service';
 
 @Component({
-  selector: 'movie-details',
-  templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+    selector: 'movie-details',
+    templateUrl: './movie-details.component.html',
+    styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-  @Input() movie: any = {};
+    @Input() movie: any = {};
+    user: User | any;
+    watchlist: MovieDto[] = [];
+    inWatchlist: boolean = false;
 
-  constructor() { }
+    constructor(private movieService: MovieService) { }
 
-  ngOnInit(): void {}
+    ngOnInit(): void {
+        if(localStorage.getItem("loggedUser") !== null)
+            this.user = JSON.parse(localStorage.getItem("loggedUser")!);
+        
+        this.movieService.getWatchlist().subscribe(data => {
+            this.watchlist = data;
+            for(let m of this.watchlist)
+                if(this.movie.id == m.id){
+                    this.inWatchlist = true;
+                    break;
+                }
+        });
+    }
 
+    addToWatchlistHandler() {
+        this.movieService.addMovieToWatchlist(this.movie);
+        this.inWatchlist = true;
+    }
+
+    removeFromWatchlistHandler() {
+        this.movieService.removeMovieFromWatchlist(this.movie);
+        this.inWatchlist = false;
+    }
 }
