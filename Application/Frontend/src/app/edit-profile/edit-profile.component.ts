0e2a8a4a05804f55b2eba9f50e01808143673829
Marija Klještan <marijakljestan@uns.ac.rs>
2022-05-26 23:48:45 +0200

    feat: Change password form validation

--- Application/Frontend/src/app/edit-profile/edit-profile.component.ts
+++ Application/Frontend/src/app/edit-profile/edit-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user.model';
 import { UserService } from '../service/user.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../service/authentication.service';
 import { ChangePassword } from '../model/change-password.model';
 
@@ -18,13 +17,8 @@ export class EditProfileComponent implements OnInit {
         currentPassword: "",
         newPassword: ""
     }
+    confirmPassword: string = "";
     displayChangePasswordForm: boolean = false;
-
-    changePasswordForm = new FormGroup({
-        currentPassword : new FormControl('', [Validators.required, Validators.minLength(6)]),
-        newPassword :     new FormControl('', [Validators.required, Validators.minLength(6)]),
-        confirmPassword : new FormControl('', [Validators.required, Validators.minLength(6)]),
-    })
 
     constructor(private userService: UserService, private authService: AuthenticationService) { }
 
